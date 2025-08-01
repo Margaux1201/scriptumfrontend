d62cb8357fbfb65f3993e79f0859a94ab3d00ac5
Margaux <margaux.courageux@gmail.com>
2025-08-01 19:34:28 +0200

    3e initialisation du frontend en react typescript et début du composant header

---
+++ pages/browse.tsx
@@ -0,0 +1,3 @@
+function Browse() {}
+
+export default Browse;
---
+++ theme.ts
@@ -0,0 +1,29 @@
+type Colors = {
+  background: string;
+  primary: string;
+  accent: string;
+  accent2: string;
+  text1: string;
+  text2: string;
+};
+
+type Fonts = {
+  title: string;
+  hs: string;
+  body: string;
+};
+
+export const colors = {
+  background: "#FBF5F3",
+  primary: "#E28413",
+  accent: "#107E7D",
+  accent2: "#CB3138",
+  text1: "#0F0F0F",
+  text2: "#FBF5F3",
+};
+
+export const fonts = {
+  title: "Hind",
+  hs: "Overlock",
+  body: "Mukta_Vaani",
+};
